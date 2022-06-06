6876b4d800f21c5a3ab97e1add1341cfabad8f78
Sasa-saska <99489644+Sasa-saska@users.noreply.github.com>
2022-06-07 00:10:54 +0300

    1

--- cache.go
+++ cache.go
@@ -1,23 +1,67 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
+	ma map[string]Data
 }
 
-func NewCache() Cache {
-	return Cache{}
+type Data struct {
+	val         string
+	hasDeadline bool
+	deadline    time.Time
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func NewCache() *Cache {
+	return &Cache{
+		make(map[string]Data),
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (cash *Cache) Put(key, value string) {
+	data := Data{
+		val:         value,
+		hasDeadline: false,
+	}
+	cash.ma[key] = data
 }
 
-func (receiver) Keys() []string {
+func (cash *Cache) PutTill(key, value string, deadline time.Time) {
+	data := Data{
+		val:         value,
+		hasDeadline: true,
+		deadline:    deadline,
+	}
+	cash.ma[key] = data
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cash *Cache) Get(key string) (string, bool) {
+	startTime := time.Now()
+
+	data, ok := cash.ma[key]
+	if !ok {
+		return "", ok
+	}
+
+	if data.hasDeadline {
+		notExp := startTime.Before(data.deadline)
+		if notExp {
+			return data.val, true
+		} else {
+			delete(cash.ma, key)
+			return "", false
+		}
+	}
+	return data.val, ok
+}
+
+func (cash *Cache) Keys() []string {
+	var array []string
+	for key := range cash.ma {
+		array = append(array, key)
+
+	}
+	return array
 }
